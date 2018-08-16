daa8fe039d63bc9a251d032d009418890feebdfd
J. Simplicio <jessicasimplicio1@gmail.com>
2018-08-16 17:07:19 -0300

    arquivo separado configuracao firebase

---
+++ js/firebase-config.js
@@ -0,0 +1,9 @@
+var config = {
+  apiKey: "AIzaSyBs90V_9KBiTkwsGGoiGpRulhamdLIW1rM",
+  authDomain: "universo-down21.firebaseapp.com",
+  databaseURL: "https://universo-down21.firebaseio.com",
+  projectId: "universo-down21",
+  storageBucket: "universo-down21.appspot.com",
+  messagingSenderId: "109037024203"
+};
+firebase.initializeApp(config);
